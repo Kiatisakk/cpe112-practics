765a5831bf305f34164de4483aaaebea8213c059
Kiatisak <kiatisakforstudy@gmail.com>
2025-05-11 00:27:17 +0700

    last EP2

--- MainMineCraftEP2.java
+++ MainMineCraftEP2.java
@@ -1,6 +1,6 @@
 import java.util.*;
 
-public class MainMineCraftEP2 {
+public class MainMineCraftEp2 {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
         int n = scan.nextInt();
@@ -16,7 +16,7 @@ public class MainMineCraftEP2 {
         int z = scan.nextInt();
         MyAlgorithm algo = new MyAlgorithm();
         int MatAns[][] = algo.floydMat(graph);
-        if (MatAns[s][vil]>MatAns[z][vil]) {
+        if (MatAns[s][vil]<=MatAns[z][vil]) {
             System.out.println("SAFE");
         } else{
             System.out.println("TOO Late");
